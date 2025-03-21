b61af630b2d3ffacb75b5033857896ba12ae7e00
GafurCodes <83255459+GafurCodes@users.noreply.github.com>
2025-03-21 02:46:07 -0400

    Updated env variables for vite

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,22 +4,22 @@ import './index.css';
 import App from './App.jsx';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-const domain = import.meta.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = import.meta.env.REACT_APP_AUTH0_CLIENT_ID;
-const audience = import.meta.env.REACT_APP_AUTH0_AUDIENCE;
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
 createRoot(document.getElementById('root')).render(
-	<StrictMode>
-		<Auth0Provider
-			domain={domain}
-			clientId={clientId}
-			authorizationParams={{
-				redirect_uri: window.location.origin,
-				audience: audience,
-				scope: 'openid profile email',
-			}}
-		>
-			<App />
-		</Auth0Provider>
-	</StrictMode>
+  <StrictMode>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: audience,
+        scope: 'openid profile email',
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </StrictMode>
 );
